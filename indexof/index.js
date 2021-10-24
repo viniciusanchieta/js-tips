389e6fff27d444dc9cafa4c9103d76518659d0c3
vinicius <vinicius@houpa.app>
2021-10-24 00:03:33 -0300

    feat: tip of indexof

---
+++ indexof/index.js
@@ -0,0 +1,8 @@
+var array = ["backend", "frontend"];
+
+console.log(array.indexOf("backend")); // 0
+// A palavra backend foi encontrada na posição 0 do array
+console.log(array.indexOf("database")); // -1
+// A palavra database não foi encontrada no array
+
+
