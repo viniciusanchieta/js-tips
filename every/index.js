b07231a5707854f0b0fb4cb2c40bf906d7c5af18
vinicius <vinicius@houpa.app>
2021-11-06 17:00:09 -0300

    feat: tip of every

---
+++ every/index.js
@@ -0,0 +1,9 @@
+const array = [10, 10, 10];
+
+const check = (currentValue) => currentValue == 10;
+// Função que verifica se o valor é igual a 10
+
+console.log(array.every(check));
+// Retorna true, pois todos os valores são iguais a 10
+
+
