4852a745b946f4d251c3a93cb3df50c85d6e5711
vinicius <vinicius@houpa.app>
2021-12-12 00:15:03 -0300

    feat: Tip of set object

---
+++ set/index.js
@@ -0,0 +1,6 @@
+const acoes = ['curtir', 'compartilhar', 'comentar', 'curtir'];
+const removerDuplicados = new Set(acoes);
+
+console.log(removerDuplicados); // Set { 'curtir', 'compartilhar', 'comentar' }
+
+
