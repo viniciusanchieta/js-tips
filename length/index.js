b19b46917daf1032c74b9d9e7edf55d7e055493d
Vinicius Anchieta <vinicius.anchieta@outlook.com>
2022-01-23 13:45:10 -0300

    feat: Tip of length

---
+++ length/index.js
@@ -0,0 +1,10 @@
+const string = "Contando caracteres com JavaScript";
+
+console.log(string.length);
+
+const array = ["front-end", "back-end", "full stack"];
+
+console.log(array.length); // 3
+
+
+
