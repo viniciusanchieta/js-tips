c971cc56894f50b52cb1b6b095918277e7873ae6
Vinicius Anchieta <vinicius.anchieta@outlook.com>
2022-02-08 00:29:54 -0300

    feat: Tip of substring

---
+++ substring/index.js
@@ -0,0 +1,19 @@
+/* 
+	O método recebe dois parâmetros, substring(inicio, fim).
+	Para extrair, imagine que cada caractere é uma posição.
+	O primeiro parâmetro é a posição inicial.
+	O segundo parâmetro é a posição final.
+*/
+
+const string = "Desenvolvimento";
+
+// Vamos limitar a exibição da string, da posição 0 até a posição 5
+console.log(string.substring(0, 5)); // Desen
+
+// Agora vamos exibir do posição 5 até o final
+console.log(string.substring(5, 15)); // volvimento
+// Ou
+console.log(string.substring(5)); // volvimento
+
+
+
