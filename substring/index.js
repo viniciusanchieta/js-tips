b36042d7c6c0b5bb31fd0629c757253b1358cacc
Vinicius Anchieta <vinicius.anchieta@outlook.com>
2022-02-15 00:52:07 -0300

    feat: Tip of substring

--- substring/index.js
+++ substring/index.js
@@ -10,7 +10,7 @@ const string = "Desenvolvimento";
 // Vamos limitar a exibição da string, da posição 0 até a posição 5
 console.log(string.substring(0, 5)); // Desen
 
-// Agora vamos exibir do posição 5 até o final
+// Agora vamos exibir da posição 5 até o final
 console.log(string.substring(5, 15)); // volvimento
 // Ou
 console.log(string.substring(5)); // volvimento
